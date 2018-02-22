241b33d263890d83719a9d99db39b47043019f98
Vahid Mahdiun <mr_vm_star@yahoo.com>
2018-02-22 18:39:50 +0330

    Give execution access to vag sh file

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ downloadNecessaryFiles(){
     curl -o /etc/vag/nginx.conf 'https://raw.githubusercontent.com/nerdial/Vagabond/master/src/etc/vag/nginx.conf'
     curl -o /etc/vag/php.ini 'https://raw.githubusercontent.com/nerdial/Vagabond/master/src/etc/vag/php.ini'
     curl -o /usr/bin/vag 'https://raw.githubusercontent.com/nerdial/Vagabond/master/vag.sh'
+    chmod +x /usr/bin/vag
 }
 
 if appExist curl ; then

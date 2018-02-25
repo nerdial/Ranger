0d0a22a5e916cb51fdf1b0fbe2b179b218180a78
Vahid Mahdiun <mr_vm_star@yahoo.com>
2018-02-25 22:23:12 +0330

    Add option feature such --help, --version , --server , --port

--- vag.sh
+++ vag.sh
@@ -1,63 +1,112 @@
 #!/bin/bash
-function portIsNumber () {
+vagabondVersion=0.1.0
+
+showHelp () {
+    echo 'List of options you may run :'
+    echo '----------------------------------------------------------------------------------'
+    echo '      -h or --help       shows the help manual'
+    echo '      -s or --server     defines which host project should run on, default=localhost'
+    echo '      -p or --port       application port  ,default=8000'
+    echo '      -d or --default    use php built-in webserver instead'
+    echo '      -r or --root       set application root ,default=current directory'
+    echo '      -v or --version    shows the version of vagabond'
+    echo '      -i or --ini        specify .ini file for php-fpm or php built-in server ,default=looks for php.ini in the current directory,'  
+    echo '                         if could not find any, i will use default file located in /etc/vag/php.ini'                                
+    echo '----------------------------------------------------------------------------------'
+}
+
+portIsNumber () {
     regex='^[0-9]+([.][0-9]+)?$'
     if ! [[ $1 =~ $regex ]] ; then
-        echo "error: Not a number" >&2; exit 1
+        echo "error: specified port is not a valid number" >&2; exit 
     else
         return 1
     fi
 }
-function appExist () {
+
+appExist () {
     type "$1" &> /dev/null ;
 }
 
-function installNginx () {
+installNginx () {
     nginx=stable
     add-apt-repository ppa:nginx/$nginx
     apt-get update
     apt-get install nginx -y
 }
 
-function installPHP () {
+installPHP () {
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
     add-apt-repository ppa:ondrej/php
     apt-get update
     apt-get install php7.2-fpm php7.2-cli php-7.2-zip php-7.2-mbstring php-7.2-dom -y
 }
 
-function installSoftwarePropertiesCommon () {
+installSoftwarePropertiesCommon () {
      apt-get install software-properties-common -y
+}
+
+printVersion () {
+    echo  "Vagabond" $vagabondVersion
 }
 
 defaultPort=8000
 defaultHost='localhost'
-if [ -z ${1+x} ]; then 
-    host=$defaultHost
-else 
-    host=$1 
-fi
 
-if [ -z ${2+x} ]; then 
-    port=$defaultPort
-else 
-    portIsNumber $2
-    port=$2 
-fi
+while :; do
+    case $1 in
+
+        -h|--help)
+            showHelp    # Display the help manual.
+            exit
+            ;;
+
+        -v|--version)
+            printVersion    # Show version of vagabond.
+            exit
+            ;;   
+
+        -s|--server)
+            if [ "$2" ]; then
+                defaultHost=$2
+                shift
+            else
+                die 'ERROR: "--server" requires a non-empty option argument.'
+            fi
+            ;; 
+        -p|-\?|--port)
+            if [ "$2" ]; then
+                portIsNumber $2
+                defaultPort=$2
+                shift
+            else
+                die 'ERROR: "--port" requires a non-empty option argument.'
+            fi
+            ;; 
+        -?*)
+            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+            exit
+            ;;
+        *)               # Default case: No more options, so break out of the loop.
+            break
+    esac
+    shift
+done
 
 if appExist add-apt-repository ; then
-    echo "add-apt-repository is installed"
+    #echo "add-apt-repository is installed"
 else
     installSoftwarePropertiesCommon    
 fi
 
 if appExist nginx ; then
-    echo "nginx is installed"
+    #echo "nginx is installed"
 else
     installNginx    
 fi
 
 if appExist php-fpm7.2 ; then
-    echo "php-fpm is installed"
+    #echo "php-fpm is installed"
 else
     installPHP    
 fi
@@ -70,7 +119,7 @@ applicationPort='{applicationPort}'
 hostName='{hostName}'
 
 modifiedPath="${PWD/////\\}"
-cat /etc/vag/nginx.conf | sed "s|"$absoulteRootPath"|"$PWD"|g" | sed "s|"$applicationPort"|"$port"|g" | sed "s|"$hostName"|"$host"|g" > /etc/nginx/conf.d/$configName".conf"
+cat /etc/vag/nginx.conf | sed "s|"$absoulteRootPath"|"$PWD"|g" | sed "s|"$applicationPort"|"$defaultPort"|g" | sed "s|"$hostName"|"$defaultHost"|g" > /etc/nginx/conf.d/$configName".conf"
 
 if (( $(ps -ef | grep -v grep | grep php7.2-fpm | wc -l) > 0 )); then
     /usr/sbin/service php7.2-fpm restart
@@ -83,7 +132,6 @@ if (( $(ps -ef | grep -v grep | grep nginx | wc -l) > 0 )); then
 else
     /usr/sbin/nginx
 fi
-urlPath="http://localhost:"$port"/"
+
+urlPath="http://localhost:"$defaultPort"/"
 python -mwebbrowser $urlPath
-
-
